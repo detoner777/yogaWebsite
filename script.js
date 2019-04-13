b9e4bc2b82766446d678c54f57d5b4c890a84ee3
Detoner <detoner777@gmail.com>
2019-04-13 19:21:11 +0300

    upgrade counter fitch - 00:00:00 when done

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 window.addEventListener("DOMContentLoaded", function() {
     'user strict';
+    //3 tabs
     let tab = document.querySelectorAll('.info-header-tab'),
         info = document.querySelector('.info-header'),
         tabContent = document.querySelectorAll('.info-tabcontent');
@@ -32,5 +33,52 @@ window.addEventListener("DOMContentLoaded", function() {
                  }
              }
          });
-    });
-    
+      
+
+
+        //timer
+        let deadline = "2019-04-13";
+        function getTimeRemaining(endtime) {
+                let t = Date.parse(endtime) - Date.parse(new Date()),
+                seconds = Math.floor((t/1000) % 60),
+                minutes = Math.floor((t/1000/60) % 60),
+                hours = Math.floor((t/(1000*60*60)));
+
+            return {
+                'total' : t,   
+                'hours'  : hours,
+                'minutes' : minutes, 
+                'seconds' : seconds
+                };
+        }
+
+        function pad(n) {
+            if (n < 10)
+                return "0" + n;
+                                
+             return n;
+        }
+          
+        function setClock (id, endtime) {
+            let timer = document.getElementById(id),
+                hours = timer.querySelector('.hours'),
+                minutes = timer.querySelector('.minutes'),
+                seconds = timer.querySelector('.seconds');
+                timeInterval = setInterval(updateClock, 1000);
+
+        function updateClock() {
+                let t = getTimeRemaining(endtime);
+                hours.textContent = pad(t.hours);
+                minutes.textContent =pad(t.minutes);
+                seconds.textContent = pad(t.seconds);
+
+                if (t.total <= 0) {
+                    clearInterval(timeInterval);
+                    hours.textContent = "00";
+                    minutes.textContent = "00";
+                    seconds.textContent = "00";
+                  }
+             }
+        }
+        setClock('timer', deadline);
+});
--- tempCodeRunnerFile.js
+++ tempCodeRunnerFile.js
@@ -1,12 +1,13 @@
 info.addEventListener('click', function (event) {
              let target = event.target;
-             if (target $$ target.classList.contains('info-header-tab')) {
+             if (target && target.classList.contains('info-header-tab')) {
                  for (let i = 0; i < tab.length; i++) {
                      if (target  === tab[i]) {
                          hideTabContent(0);
                          showTabContent(i);
                          break;
                      }
-                 }
-             }
-         });
+                    }
+                }
+                 } );
+          
