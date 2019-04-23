bde6cf31e67cf7d4b6e2d6ed54a0acb222d83132
Detoner <detoner777@gmail.com>
2019-04-23 08:04:39 +0300

    add Slider and Calc with litel bug

--- script.js
+++ script.js
@@ -171,10 +171,116 @@ close.addEventListener ('click', function () {
         }
 
     });
+
+    //Slider
+    
+    let slideIndex = 1,
+        slides = document.querySelectorAll('.slider-item'),
+        prev = document.querySelector('.prev'),
+        next = document.querySelector('.next'),
+        dotsWrap = document.querySelector('.slider-dots'),
+        dots = document.querySelectorAll('.dot');
+
+        showSlides(slideIndex);
+
+    function showSlides(n) {
+        // --- привъязка  слайдов к точкам
+        if (n > slides.length) {
+            slideIndex = 1;
+        }
+        if (n < 1) {
+            slideIndex = slides.length;
+        }
+        // ---
+
+        // >> прячем слайды в один и привъязываем к 1вой точке
+        slides.forEach((item) => item.style.display = 'none');
+        //более старый способ перебора скрытия слайдов
+        //for (let i = 0; i < slides.length; i++) {
+        //   slides[i].style.display = 'none';
+        //}
+
+        dots.forEach((item) => item.classList.remove('dot-active'));
+
+        slides[slideIndex - 1].style.display = 'block';
+        dots[slideIndex - 1].classList.add('dot-active');
+        // >> 
+    }
+
+    function plusSlides(n) {
+        showSlides(slideIndex += n);
+    }
+    function curentSlide(n) {
+        showSlides(slideIndex = n);
+    }
+
+    prev.addEventListener('click', function () {
+        plusSlides(-1);
+    });
+    next.addEventListener('click', function () {
+        plusSlides(1);
+    });
+
+    dotsWrap.addEventListener('click', function(event) {
+        for (let i = 0; i < dots.length + 1; i++) {
+           if (event.target.classList.contains('dot') && event.target == dots[i-1]) {
+               curentSlide(i);
+           }  
+        }
+    });
+
+    //Calculator
+    let persons = document.querySelectorAll('.counter-block-input')[0],
+        restDays = document.querySelectorAll('.counter-block-input')[1],
+        place = document.getElementById('select'),
+        totalValue = document.getElementById('total'),
+        personsSum = 0,
+        daysSum = 0,
+        total = 0;
+
+        totalValue.innerHTML = 0;
+
+        
+        
+        persons.addEventListener('change', function() {
+            personsSum = +this.value;
+            
+            total = (daysSum + personsSum)*4000;
+               
+                if(restDays.value == ''  ) {
+                totalValue.innerHTML = 0;
+            } else {
+                totalValue.innerHTML = total;
+            }
+        });
+
+        
+        restDays.addEventListener('change', function() {
+            daysSum = +this.value;
+                             
+           total = (daysSum + personsSum)*4000;
+                                    
+            if(persons.value == '' ) {
+                totalValue.innerHTML = 0;
+            } else {
+                totalValue.innerHTML = total;
+            }
+        });  
+        
+        place.addEventListener('change', function() {
+            if (restDays.value == '' ) {
+                totalValue.innerHTML = 0;
+            } else {
+                let a = total;
+                totalValue.innerHTML = a * this.options[this.selectedIndex].value;
+            }
+
+          });
+
+          //have a bug in a calc
+
+         
 });
-
-
-
 
 
 
